f04a331a7ff8d93b4fcf848ecde28b53550b9cff
mohamadlounnas <mohamadlounnas@gmail.com>
2024-09-09 20:44:30 +0100

    Refactor import statement in feeef.ts and update baseURL

--- src/feeef.ts
+++ src/feeef.ts
@@ -2,7 +2,7 @@ import { FeeeF } from "feeef";
 
 export const ff = new FeeeF({
     apiKey: "c43Yfd3bgolijJU3b3bx095vlfTrvnL94baZrd1",
-    baseURL: "https://apis.feeef.net/api/v1",
+    baseURL: "https://feeef.app/api/v1",
     cache: 10,
     // baseURL: "http://localhost:3333/api/v1",
   });
